e1bae6124e0a9c8545451d08580fc9b6033b1c35
Aman Sharma <aman8696.56@gmail.com>
2020-04-27 06:16:30 +0530

    install vue router | Foo Bar components for example | History mode

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Foo from './components/Foo.vue'
+import Bar from './components/Bar.vue'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+let routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+let router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
